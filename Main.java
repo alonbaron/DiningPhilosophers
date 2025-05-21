cbb356869dbdba5abda5ccd008486a234bcdfaf2
Alon Baron <alonbaron123@gmail.com>
2025-05-21 11:59:48 +0300

    commit

--- Main.java
+++ Main.java
@@ -3,13 +3,23 @@ public class Main {
     public static void main(String[] args) {
         Fork fork1 = new Fork(1);
         Fork fork2 = new Fork(2);
-        Philosopher philosopher1 = new Philosopher("John Lock", fork1, fork2);
-        Philosopher philosopher2 = new Philosopher("Plato", fork2, fork1);
+        Fork fork3 = new Fork(3);
+        Fork fork4 = new Fork(4);
+        Fork fork5 = new Fork(5);
+
+        Philosopher philosopher1 = new Philosopher("P1", fork1, fork2);
+        Philosopher philosopher2 = new Philosopher("P2", fork2, fork3);
+        Philosopher philosopher3 = new Philosopher("P3", fork3, fork4);
+        Philosopher philosopher4 = new Philosopher("P4", fork4, fork5);
+        Philosopher philosopher5 = new Philosopher("P5", fork5, fork1);
         new Thread(() -> {
             while (true) {
                 System.out.println(philosopher1);
                 System.out.println(philosopher2);
-                Utils.sleep(1000);
+                System.out.println(philosopher3);
+                System.out.println(philosopher4);
+                System.out.println(philosopher5);
+                Utils.sleep(10000);
             }
         }).start();
     }
